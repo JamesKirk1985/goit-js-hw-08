14a4d06deca8478aaf595bf3eac7a355242df832
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-13 01:55:51 +0300

    02 Funising Second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+console.log(throttle);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+function stopVideoTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    // console.log('seconds:', seconds);
+  });
+}
+var throttle = require('lodash.throttle');
+player.on('timeupdate', throttle(stopVideoTime, 1000));
